e0c19d1038e2644deb36f7e4da7758d42617f778
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-10-28 12:52:17 -0300

    Update auth.ts

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -95,4 +95,56 @@ router.post("/sair", (req, res) => {
 	res.clearCookie("pid")
 })
 
+router.get("/data", (req, res) => {
+	const pid = req.cookies["pid"]
+
+	if (typeof pid !== "string") {
+		res.status(400)
+			.json({"mensagem": "usuário não logado"})
+		return
+	}
+
+	Usuario.findOne( { where: { pid } } )
+	.then( (data) => {
+		if (data == null) {
+			res.status(404)
+				.json({"erro": "usuário não encontrado no banco de dados"})
+		}
+		else {
+			res.status(200)
+				.json(data)
+		}
+	})
+	.catch(_ => {
+		res.status(500)
+			.json({"erro": "erro na procura de usuarios no banco de dados"})
+	})
+})
+
+router.get("/login-get/:pid", (req, res) => {
+	const pid = req.params["pid"]
+
+	if (typeof pid !== "string") {
+		res.status(400)
+			.json({"mensagem": "usuário não logado"})
+		return
+	}
+
+	Usuario.findOne( { where: { pid } } )
+	.then( (data) => {
+		if (data == null) {
+			res.status(404)
+				.json({"erro": "usuário não encontrado no banco de dados"})
+		}
+		else {
+			res.status(200)
+				.json(data)
+		}
+	})
+	.catch(_ => {
+		res.status(500)
+			.json({"erro": "erro na procura de usuarios no banco de dados"})
+	})
+})
+
 export default router
