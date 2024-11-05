1e124329cab8dc3e791dca4ae276af0d6b77091c
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-11-04 23:28:13 -0300

    i hate tests

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -11,31 +11,3 @@ describe("check API", () => {
 	})
 })
 
-describe("Autenticação", () => {
-	/*
-	const data = {
-		pid: 'f3da7ccb-8f74-4b54-a3d7-a92e9a177798',
-		login: '0011',
-		nome: 'teste',
-		senha: 'teste',
-		email: 'teste@email.com',
-		telefone: '11011223344',
-		cpf: '01001001011',
-		admin: false
-	}
-
-	let token = null
-
-	test('[POST] /auth/cadastrar', async () => {
-		const response = await axios.post(`${LINK}/auth/cadastrar`, data)
-		expect(response.status).toBe(200)
-	})
-	*/
-
-	/*
-	test('[POST] /auth/login', async () => {
-		let response = await axios.post(`${LINK}/auth/login`, { nome: data.nome, senha: data.senha }, { withCredentials: true })
-		expect(response.status).toBe(200)
-	})
-	*/
-})
