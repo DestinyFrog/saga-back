49849c3ddf6e0242841d70152eb3b5bae05eff78
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-11-05 21:00:49 -0300

    use type in middlewares

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,47 +1,102 @@
 import jwt from 'jsonwebtoken'
-import Usuarios from '../models/usuario'
+import Usuarios from '../models/usuario.js'
+import { Request, Response, NextFunction } from 'express'
 
-export function processJWT(req, res, next) {
-	try {
-		const token = req.cookies?.jwt
-		if (!token)
-			throw new Error("JWT não encontrado")
-
-		var decoded = jwt.verify(token, process.env["SECRET"] as string)
-		req.jwt = decoded["jwt"]
-	} catch(err) {
+export function processJWT(req:Request, res:Response, next:NextFunction) {
+	const token = req.cookies?.jwt
+	if (!token) {
 		res.status(400)
-			.json({"mensagem": "erro ao ler JWT"})
+			.json({"mensagem": "erro ao encontrar JWT"})
+		return
 	}
-	finally {
-		next()
-	}
+
+	var decoded = jwt.verify(token, process.env["SECRET"] as string)
+	req["jwt"] = decoded["jwt"]
+	next()
 }
 
-export function checkAuthentication(req:any, res:any, next:any) {
-	const token = req?.jwt
-	if (!token)
-		throw new Error("Not Logged")
+export function checkAuthentication(req:Request, res:Response, next:NextFunction) {
+	const token = req["jwt"]
+	if (!token) {
+		res.status(400)
+			.json({"mensagem": "erro ao encontrar JWT"})
+		return
+	}
 
 	Usuarios.findOne( { where: { pid: token } } )
 	.then( (data) => {
 		if (data == null) {
 			res.status(404)
-				.json({"erro": "usuário não encontrado no banco de dados"})
+				.json({"mensagem": "usuário não encontrado no banco de dados"})
 		}
 		else {
-			req.data = data
+			req["data"] = data
 			next()
 		}
 	})
 	.catch(err => {
 		console.error(err)
 		res.status(500)
-			.json({"erro": "erro na procura de usuarios no banco de dados"})
+			.json({d: err,"mensagem": "erro na procura de usuarios no banco de dados"})
 	})
 }
 
-export function checkAdmin(req:any, res:any, next:any) {
-	console.log(req.data)
+export function checkAdmin(req:Request, res:Response, next:NextFunction) {
+	const data = req["data"]
+
+	if (!data) {
+		res
+			.status(400)
+			.json({"mensagem": "erro ao encontrar dados do usuário"})
+		return
+	}
+
+	if (data.acesso != "admin") {
+		res
+			.status(403)
+			.json({"mensagem": "usuário não é administrador"})
+		return
+	}
+	
+	next()
+}
+
+export function checkTecnico(req:Request, res:Response, next:NextFunction) {
+	const data = req["data"]
+
+	if (!data) {
+		res
+			.status(400)
+			.json({"mensagem": "erro ao encontrar dados do usuário"})
+		return
+	}
+
+	if (data.acesso != "tecnico") {
+		res
+			.status(403)
+			.json({"mensagem": "usuário não é técnico"})
+		return
+	}
+	
+	next()
+}
+
+export function checkPeao(req:Request, res:Response, next:NextFunction) {
+	const data = req["data"]
+
+	if (!data) {
+		res
+			.status(400)
+			.json({"mensagem": "erro ao encontrar dados do usuário"})
+		return
+	}
+
+	if (data.acesso != "peao") {
+		res
+			.status(403)
+			.json({"mensagem": "usuário não é peão"})
+		return
+	}
+	
 	next()
 }
