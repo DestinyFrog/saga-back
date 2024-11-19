4d71630bf15b9ca2cd73fcecb5fbd236f79b2492
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-11-18 21:25:40 -0300

    implement auth-login

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -32,7 +32,50 @@ router.post("/login", (req, res) => {
 				} else {
 					res.status(200)
 						.cookie("jwt", token)
-						.json({"jwt": token})
+						.json(data)
+				}
+			} else {
+				res.status(403)
+					.json({"mensagem": "senha incorreta"})
+			}
+		}
+
+	})
+	.catch(err => {
+		console.error(err)
+		res.status(500)
+			.json({"mensagem": "erro na procura de usuarios no banco de dados"})
+	})
+})
+
+router.post("/login-admin", (req, res) => {
+	const body = req.body
+
+	if (typeof body.login !== "string") {
+		res.status(400)
+			.json({"mensagem": "coluna 'login' indefinida"})
+		return
+	}
+
+	client.usuario.findFirst( {where: {
+		login: body.login
+	}} )
+	.then( (data) => {
+		if (data == null) {
+			res.status(404)
+				.json({"mensagem": "usuário não encontrado no banco de dados"})
+		}
+		else {
+			if ( body.senha as string === data.senha ) {
+				const token = jwt.sign( { jwt: data.pid }, process.env["SECRET"] as string )
+
+				if (data.acesso != "ADMIN") {
+					res.status(403)
+						.json({"mensagem": "usuário não é administrador"})
+				} else {
+					res.status(200)
+						.cookie("jwt", token)
+						.json(data)
 				}
 			} else {
 				res.status(403)
