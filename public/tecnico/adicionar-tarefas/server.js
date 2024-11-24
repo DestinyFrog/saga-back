938b13742896b6e3a879b68eab4fc29724636a34
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-11-24 20:23:06 -0300

    local fetch

--- public/tecnico/adicionar-tarefas/server.js
+++ public/tecnico/adicionar-tarefas/server.js
@@ -10,8 +10,8 @@ function openPage(id) {
 	document.getElementById("surname").value = obj.titulo
 
 	const now = new Date(obj.createdAt)
-	const data_inicio = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
-	document.getElementById("data-inicio") = data_inicio
+	const data_inicio = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+	document.getElementById("data-inicio").textContent = data_inicio
 
 	const el_data_vencimento = document.getElementById("data-vencimento")
 	// document.getElementById("tipo")
@@ -36,67 +36,58 @@ function openPage(id) {
 	const btn_salva = document.getElementById("btn-salva")
 
 	const now = new Date()
-	el_data_inicio.value = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
-	el_data_vencimento.value = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
+	el_data_inicio.value = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+	el_data_vencimento.value = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
 
-	try {
 
-		// const maquina_lista = document.getElementById("maquina-lista")
-		
-		let resp = await fetch(LINK+"/maquina")
-		let data = await resp.json()
 
-		// maquina_lista.innerHTML = ""
+	fetch(LINK + "/maquina")
+		.then(resp => resp.json())
+		.then(data => {
+			data.forEach(({ id, nome, descricao, codigo, Tarefa }) => {
+				el_maquina_select.innerHTML += `
+					<option value="${id}">${nome}</option>
+				`
+			})
 
-		data.forEach(({id, nome, descricao, codigo, Tarefa}) => {
-			/*
-			maquina_lista.innerHTML += `
-				<div class="maquina">
-                    <h4 id="maquina">${codigo}. ${nome}</h4>
-                    <h4 id="maquina">${descricao}</h4>
-                    <h4 id="estado">${Tarefa.length<1
-						? "OK"
-						: Tarefa[0].estado
-					}</h4>
-                </div>
-			`
-			*/
-
-			el_maquina_select.innerHTML += `
-				<option value="${id}">${nome}</option>
-			`
+		})
+		.catch(err => {
+			console.error(err)
 		})
 
-		resp = await fetch(LINK+"/equipe")
-		data = await resp.json()
-
-		data.forEach(({id, nome}) => {
-			el_equipe_select.innerHTML += `
-				<option value="${id}">${nome}</option>
-			`
+	fetch(LINK + "/equipe")
+		.then(resp => resp.json())
+		.then(data => {
+			data.forEach(({ id, nome }) => {
+				el_equipe_select.innerHTML += `
+					<option value="${id}">${nome}</option>
+				`
+			})
 		})
-			
-
-		const tarefas_lista = document.getElementById("tarefas-lista")
-		const resp2 = await fetch(LINK+"/tarefa/criadas")
-		const data2 = await resp2.json()
-	
-		data2.forEach((n) => {
-			const {id, titulo, descricao, estado} = n
-			vec.push(n)
-			tarefas_lista.innerHTML += `
-				<div class="maquina" onclick="openPage(${vec.length-1})">
-					<h4 id="maquina">${titulo}</h4>
-					<h4 id="maquina">${descricao}</h4>
-					<h4 id="estado" style="color:${ estado=="EM ANDAMENTO"?"orange":(estado=="CONCLUÍDO"?"green":"red") };">${estado}</h4>
-				</div>
-			`
+		.catch(err => {
+			console.error(err)
 		})
 
-	}
-	catch (err) {
-		console.error(err)
-	}
+	const tarefas_lista = document.getElementById("tarefas-lista")
+
+	fetch(LINK + "/tarefa/criadas")
+		.then(resp => resp.json())
+		.then(data => {
+			data.forEach((n) => {
+				const { id, titulo, descricao, estado } = n
+				vec.push(n)
+				tarefas_lista.innerHTML += `
+					<div class="maquina" onclick="openPage(${vec.length - 1})">
+						<h4 id="maquina">${titulo}</h4>
+						<h4 id="maquina">${descricao}</h4>
+						<h4 id="estado" style="color:${estado == "EM ANDAMENTO" ? "orange" : (estado == "CONCLUÍDO" ? "green" : "red")};">${estado}</h4>
+					</div>
+				`
+			})
+		})
+		.catch(err => {
+			console.error(err)
+		})
 
 	btn_salva.addEventListener('click', async (ev) => {
 		const body = {
@@ -111,15 +102,17 @@ function openPage(id) {
 			descricao: el_descricao.value
 		}
 
-		const res = await fetch(LINK+"/tarefa", {
+		fetch(LINK + "/tarefa", {
 			body: JSON.stringify(body),
 			headers: { Accept: 'application/json', "Content-Type": 'application/json' },
 			method: "POST"
 		})
-
-		const data = await res.json()
-		console.log(data) 
-	} )
+		.then(resp => resp.json())
+		.then(data => {
+			console.log(data)
+		})
+		.catch( err => console.error(err) )
+	})
 
 })
 .call(this)
