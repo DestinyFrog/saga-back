5fd4628c11de6cfbbff4aa923bbd414db71793e7
DestinyFrog <107517847+DestinyFrog@users.noreply.github.com>
2024-11-24 16:37:11 -0300

    update tarefa system front

--- public/func/script.js
+++ public/func/script.js
@@ -1,4 +1,48 @@
 
+const dialog_resposta = document.getElementById("dialog-resposta")
+dialog_resposta.style.display = 'none'
+
+const but_fechar_dialog = document.getElementById("resposta-fechar")
+but_fechar_dialog.addEventListener('click', (ev) => {
+	dialog_resposta.style.display = 'none'
+})
+
+const tarefa_descricao = document.getElementById("tarefa-descricao")
+tarefa_descricao.style.display = 'none'
+
+const but_fechar = document.getElementById("butFechar")
+
+const resposta_submit = document.getElementById("resposta-submit")
+resposta_submit.addEventListener('click', () => {
+
+	const id = document.getElementById('resposta_subtarefa').value
+	
+	const body = {
+		titulo: document.getElementById("resposta-titulo").value,
+		descricao: document.getElementById("resposta-descricao").value,
+		alerta: document.getElementById("resposta-alerta").checked
+	}
+
+	fetch(LINK+"/tarefa/resposta/"+id, {
+		body: JSON.stringify(body),
+		headers: { Accept: 'application/json', "Content-Type": 'application/json' },
+		method: "POST"
+	})
+	.then(resp => resp.json())
+	.then(data => {
+		console.log(data)
+		dialog_resposta.style.display = 'none'
+		location.reload()
+	})
+	.catch(err => {
+		console.error(err)
+	})
+})
+
+but_fechar.addEventListener('click', (ev) => {
+	tarefa_descricao.style.display = 'none'
+})
+
 const vec = []
 
 async function b() {
@@ -7,8 +51,6 @@ async function b() {
 	const resp = await fetch(LINK+"/tarefa")
 	const data = await resp.json()
 
-	console.log(data)
-
 	data.forEach((n) => {
 		const {id, titulo, descricao, estado} = n
 		vec.push(n)
@@ -16,7 +58,7 @@ async function b() {
 			<div class="maquina" onclick="ch(${vec.length-1})">
 				<h4 id="maquina">${titulo}</h4>
 				<h4 id="maquina">${descricao}</h4>
-				<h4 id="estado">${estado}</h4>
+				<h4 id="estado" style="color:${ estado=="EM ANDAMENTO"?"orange":(estado=="CONCLUÍDO"?"green":"red") };">${estado}</h4>
 			</div>
 		`
 	})
@@ -25,7 +67,8 @@ async function b() {
 b()
 
 function ch(n) {
-	const {titulo, maquina, createdAt, criador, tipo, estado, criticidade, descricao} = vec[n]
+	tarefa_descricao.style.display = 'block'
+	const {titulo, maquina, createdAt, criador, tipo, estado, criticidade, descricao, SubTarefa} = vec[n]
 
 	document.getElementById("nome-maquina").textContent = titulo
 	document.getElementById("surname").textContent = maquina.nome
@@ -34,7 +77,34 @@ function ch(n) {
 	document.getElementById("data-exi").textContent = `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`
 	document.getElementById("responsavel").textContent = criador.nome
 	document.getElementById("tipo-ex").textContent = tipo
+
 	document.getElementById("processo-ex").textContent = estado
+	console.log(estado)
+	if (estado == "EM ANDAMENTO") {
+		document.getElementById("processo-ex").style.color = "orange"
+	}
+	else if (estado == "CONCLUÍDO") {
+		document.getElementById("processo-ex").style.color = "green"
+	}
+
 	document.getElementById("criticidade-ex").textContent = criticidade
-	document.getElementById("descricao-ex").value = descricao
+	document.getElementById("descricao-ex").textContent = descricao
+
+	const subs = SubTarefa.map((sub) => {
+		console.log(sub)
+		return `
+			<li class="subtarefa-con">
+				<h3>${sub.titulo}</h3>
+				<hr>
+				<p>${sub.descricao}</p>
+				<p>Concluído: ${sub.concluido?"✅":"❌"}</p>
+				<h3>Respostas</h3>
+				<button onclick="
+					document.getElementById('dialog-resposta').style.display='block';
+					document.getElementById('resposta_subtarefa').value = ${sub.id};
+				">Responder</button>
+			</li>
+		`
+	})
+	document.getElementById("subtarefas").innerHTML = subs.join("")
 }
